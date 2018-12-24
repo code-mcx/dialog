6c8160c9a7f60690139cabbf18ab8563f151dfd1
code-mcx <1827939333@qq.com>
2018-12-24 13:05:44 +0800

    Format code rollup :strawberry:

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,19 +1,19 @@
 function getOffsetTop(dom) {
-  	let top = dom.offsetTop;
-  	while (dom.offsetParent !== null) {
-  		top += dom.offsetParent.offsetTop;
-  		dom = dom.offsetParent;
-  	}
-	return top;
+  let top = dom.offsetTop;
+  while (dom.offsetParent !== null) {
+    top += dom.offsetParent.offsetTop;
+    dom = dom.offsetParent;
+  }
+  return top;
 }
 
 function getOffsetLeft(dom) {
-  	let left = dom.offsetLeft;
-  	while (dom.offsetParent !== null) {
-  		left += dom.offsetParent.offsetLeft;
-  		dom = dom.offsetParent;
-  	}
-	return left;
+  let left = dom.offsetLeft;
+  while (dom.offsetParent !== null) {
+    left += dom.offsetParent.offsetLeft;
+    dom = dom.offsetParent;
+  }
+  return left;
 }
 
-export {getOffsetLeft, getOffsetTop}
+export { getOffsetLeft, getOffsetTop }
